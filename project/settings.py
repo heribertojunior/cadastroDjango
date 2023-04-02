7944dacdabb6ff55dd80706251237c39dae84774
HeribertoJunior <44383351+heribertojunior@users.noreply.github.com>
2023-04-02 13:57:54 -0400

    ajeitando settings

--- project/settings.py
+++ project/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'agora_vai',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django_psdb_engine',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+  }
 }
 
 
